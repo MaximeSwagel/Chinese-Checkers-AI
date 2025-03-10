7e3d0475b4d8a69ea5538b228dbb434cf8722b8e
Magnus Ottosen <magnus@ottosen.dk>
2025-03-10 23:02:56 +0100

    Added planning

--- bit_board_logic.py
+++ bit_board_logic.py
@@ -33,3 +33,36 @@ print(f"PEXT Result: {bin(extracted)[2:]}")
 # Perform PDEP (Deposit)
 deposited = pdep(extracted, mask_bitboard)
 print(f"PDEP Result: {bin(deposited)[2:].zfill(81)}")
+
+
+"""
+Planned logic
+
+single step moves:
+neighbors and not all_pieces
+
+
+Jump moves:
+1. Use pext to extract from all_pieces using neighbors as a mask (calling this neighbor_output)
+2. Use pext to extract from all_pieces using potential_jumps as a mask (calling this jump_output)
+
+3. Use logical and for "neighbor_output and not jump_output" (calling this compressed_jumps)
+
+4. Use pdep to spread compressed_jumps using potential_jumps as a mask (calling this final_jump_moves)
+
+Keep a list of the what nodes we visit and repeat for each position in final_jump_moves
+
+
+To get full list of all moves use logical or on all the outputs
+
+
+
+apply moves could be with logical xor
+
+0001010010101001 current board
+xor
+0101000000000000 steps moved
+=
+0100010010101001 after moved
+
+"""
